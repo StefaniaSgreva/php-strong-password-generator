42836e519b6b9c80e2989bd6ca84f00566c0ae94
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-29 16:29:24 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ $lower_case = 'abcdefghijklmnopqrstuvwxyz';
 $numbers = '0123456789';
 $special_chars = '!"£$%&/()=?^é*§ç°[]{}#';
 
+function pswd_generator($length)
+{
+    $data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%&/()=?^é*§ç°[]{}#';
+    return substr(str_shuffle($data), 0, $_POST['length']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,11 +23,19 @@ $special_chars = '!"£$%&/()=?^é*§ç°[]{}#';
 
 <body>
     <form action="index.php" method="POST" name="pswdForm">
-        <label for="pswd-length">Generate Password</label>
-        <input type="number" id="pswd" name="pswd" value="password">
+        <label for="pswd-length">Password Length</label>
+        <input type="number" id="length" name="length" value="<?php echo !empty($length) ? $length : ''; ?>">
         <button type="submit">Generate</button>
     </form>
-
+    <div>
+        <?php if (empty($_POST['length'])) { ?>
+        <h4>Please enter a number for the length!</h4>
+        <?php } else { ?>
+        <h4>Your new password is:
+            <?php echo pswd_generator($_POST['length']) ?>
+        </h4>
+        <?php } ?>
+    </div>
 </body>
 
 </html>
