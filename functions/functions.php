d7a6cb67301826fa1bbe9cb7bf4ddaa8b31da4fa
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-29 19:04:51 +0100

    some style upgrades

--- functions/functions.php
+++ functions/functions.php
@@ -1,7 +1,7 @@
 <?php
 function pswd_generator($length)
 {
-    $data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%&/()=?^é*§ç°[]{}#';
+    $data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%&/()=?*#@';
     $newPswd = substr(str_shuffle($data), 0, $length);
     session_start();
     $_SESSION['userpassword'] = $newPswd;
